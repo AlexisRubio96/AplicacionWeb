d9dee510eb51b5d5dbe51548f7597e62a615c039
AlexisRubio96 <jarss96@hotmail.com>
2019-05-08 01:40:14 -0500

    First implementation SideBar & template Create Event

--- routes/root.js
+++ routes/root.js
@@ -13,7 +13,7 @@ function isLoggedIn(req, res, next) {
 }
 
 router.get('/', (req, res) => {
-    res.render('profilePrueba') ;       //Aqui va 'home'
+    res.render('createEvent') ;       //Aqui va 'home'
 });
 
 router.get('/logout', (req, res) => {
